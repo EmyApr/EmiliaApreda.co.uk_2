2a05cc0a00cdc54335371893fb2e4ef20f8ddc85
EmyApr <110528040+EmyApr@users.noreply.github.com>
2023-10-21 10:44:42 +0100

    IN PROGRESS 1

---
+++ assets/js/filterscript.js
@@ -0,0 +1,19 @@
+const filterButtons = document.querySelectorAll(".filter-buttons .filter-button");
+const filterableCards = document.querySelectorAll(".no-show");
+
+
+
+const filterCards = e => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+
+
+    filterableCards.forEach(card => {
+        card.classList.add("hide");
+        if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all"){
+            card.classList.remove("hide");
+        }
+    });
+};
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards));
